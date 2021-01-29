24312b06afe022a11b01ad6c48cdf1333e161fda
rayeshrajah <rayeshrajah@gmail.com>
2021-01-28 22:12:40 -0500

    added styles for drop down menu in IntroPage.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { useDispatch } from "react-redux";
 import { getUsers } from "./actions/userActions";
 import LandingPage from "./components/LandingPage";
 import NavigationBar from "./components/NavigationBar";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee, faGripLines } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faCoffee, faGripLines)
 
 function App() {
   const dispatch = useDispatch();
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/Navbar.scss";
-import Logo from "../images/cooknbake.jpg"
+import Logo from "../images/cooknbake.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function NavigationBar() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="navbar">
-      <div className="logo-of-app">
-        <a href="/"><img src={Logo} /></a>
-      </div>
+      <a href="/">
+        <img src={Logo}></img>
+      </a>
+      <button className="dropdown-btn-mobile" onClick={() => toggle ? setToggle(false) : setToggle(true)}>
+        <FontAwesomeIcon icon="grip-lines" size="lg" />
+      </button>
+      { toggle && 
+        <div className="dropdown-btn-content">
+          <div className="dropdown-content">
+          <button>Login</button>
+          <button>Signup</button>
+          </div>
+        </div>
+      }
     </nav>
   );
 }
